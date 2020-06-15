4c044008078c4779d7277817821ba53b1a4abc3e
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-06-15 11:02:53 +0200

    Add code to calculate PDF uncertainties

--- src/lib.rs
+++ src/lib.rs
@@ -159,3 +159,149 @@ impl Drop for Pdf {
         }
     }
 }
+
+/// Structure for storage of uncertainty info calculated over a PDF error set.
+pub struct PdfUncertainty {
+    /// The central value.
+    pub central: f64,
+    /// The unsymmetric error in positive direction.
+    pub errplus: f64,
+    /// The unsymmetric error in negative direction.
+    pub errminus: f64,
+    /// The symmetric error.
+    pub errsymm: f64,
+    /// The scale factor needed to convert between the PDF set's default confidence level and the
+    /// requested confidence level.
+    pub scale: f64,
+    /// Extra variable for separate PDF and parameter variation errors with combined sets.
+    pub errplus_pdf: f64,
+    /// Extra variable for separate PDF and parameter variation errors with combined sets.
+    pub errminus_pdf: f64,
+    /// Extra variable for separate PDF and parameter variation errors with combined sets.
+    pub errsymm_pdf: f64,
+    /// Extra variable for separate PDF and parameter variation errors with combined sets.
+    pub err_par: f64,
+}
+
+/// Class for PDF set metadata and manipulation.
+pub struct PdfSet {
+    ptr: *mut c_void,
+}
+
+impl PdfSet {
+    /// Constructor from a set name.
+    #[must_use]
+    pub fn new(setname: &str) -> Self {
+        cfg_if! {
+            if #[cfg(feature = "docs-only")] {
+                Self { ptr: ptr::null_mut::<c_void>() }
+            } else {
+                let setname = CString::new(setname).unwrap();
+                let setname_ptr = setname.as_ptr();
+
+                Self {
+                    ptr: unsafe {
+                        cpp!([setname_ptr as "const char *"] -> *mut c_void as "LHAPDF::PDFSet *" {
+                            return new LHAPDF::PDFSet(setname_ptr);
+                        })
+                    },
+                }
+            }
+        }
+    }
+
+    /// Calculate central value and error from vector values with appropriate formulae for this
+    /// set.
+    ///
+    /// Warning: The values vector corresponds to the members of this PDF set and must be ordered
+    /// accordingly.
+    ///
+    /// In the Hessian approach, the central value is the best-fit "values[0]" and the uncertainty
+    /// is given by either the symmetric or asymmetric formula using eigenvector PDF sets.
+    ///
+    /// If the PDF set is given in the form of replicas, by default, the central value is given by
+    /// the mean and is not necessarily "values[0]" for quantities with a non-linear dependence on
+    /// PDFs, while the uncertainty is given by the standard deviation.
+    ///
+    /// The argument `cl` is used to rescale uncertainties to a particular confidence level (in
+    /// percent); a negative number will rescale to the default CL for this set. The default value
+    /// in LHAPDF is `100*erf(1/sqrt(2))=68.268949213709`, corresponding to 1-sigma uncertainties.
+    ///
+    /// If the PDF set is given in the form of replicas, then the argument `alternative` equal to
+    /// `true` (default in LHAPDF: `false`) will construct a confidence interval from the
+    /// probability distribution of replicas, with the central value given by the median.
+    ///
+    /// For a combined set, a breakdown of the separate PDF and parameter variation uncertainties
+    /// is available. The parameter variation uncertainties are computed from the last `2*n`
+    /// members of the set, with `n` the number of parameters.
+    #[must_use]
+    pub fn uncertainty(&self, values: &[f64], cl: f64, alternative: bool) -> PdfUncertainty {
+        let mut central = 0.0;
+        let mut errplus = 0.0;
+        let mut errminus = 0.0;
+        let mut errsymm = 0.0;
+        let mut scale = 0.0;
+        let mut errplus_pdf = 0.0;
+        let mut errminus_pdf = 0.0;
+        let mut errsymm_pdf = 0.0;
+        let mut err_par = 0.0;
+
+        cfg_if! {
+            if #[cfg(not(feature = "docs-only"))] {
+                let self_ptr = self.ptr;
+                let values_ptr = values.as_ptr();
+                let values_len = values.len();
+
+                unsafe {
+                    cpp!([self_ptr as "LHAPDF::PDFSet *", values_ptr as "double *",
+                          values_len as "std::size_t", cl as "double", alternative as "bool",
+                          mut central as "double", mut errplus as "double",
+                          mut errminus as "double", mut errsymm as "double", mut scale as "double",
+                          mut errplus_pdf as "double", mut errminus_pdf as "double",
+                          mut errsymm_pdf as "double", mut err_par as "double"] -> () as "void" {
+                        LHAPDF::PDFUncertainty res;
+                        std::vector<double> vec_values(values_ptr, values_ptr + values_len);
+                        self_ptr->uncertainty(res, vec_values, cl, alternative);
+                        central = res.central;
+                        errplus = res.errplus;
+                        errminus = res.errminus;
+                        errsymm = res.errsymm;
+                        scale = res.scale;
+                        errplus_pdf = res.errplus_pdf;
+                        errminus_pdf = res.errminus_pdf;
+                        errsymm_pdf = res.errsymm_pdf;
+                        err_par = res.err_par;
+                    });
+                }
+            }
+        }
+
+        PdfUncertainty {
+            central,
+            errplus,
+            errminus,
+            errsymm,
+            scale,
+            errplus_pdf,
+            errminus_pdf,
+            errsymm_pdf,
+            err_par,
+        }
+    }
+}
+
+impl Drop for PdfSet {
+    fn drop(&mut self) {
+        cfg_if! {
+            if #[cfg(not(feature = "docs-only"))] {
+                let self_ptr = self.ptr;
+
+                unsafe {
+                    cpp!([self_ptr as "LHAPDF::PDFSet *"] -> () as "void" {
+                        delete self_ptr;
+                    })
+                }
+            }
+        }
+    }
+}
