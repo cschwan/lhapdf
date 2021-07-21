b5b55fe2b055d0a3ce3a91f0e0e38b04255953e0
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-07-21 17:39:44 +0200

    Migrate from `cpp` to `cxx`
    - there are still a few TODOs to fix
    - documentation will not be generated on docs.rs, because the LHAPDF
      library is missing and I removed the `docs-only` feature

--- build.rs
+++ build.rs
@@ -1,16 +1,16 @@
-#[cfg(not(feature = "docs-only"))]
 fn main() {
     let lhapdf = pkg_config::Config::new()
         .atleast_version("6")
         .probe("lhapdf")
         .unwrap();
-    let mut cpp_build = cpp_build::Config::new();
+
+    let mut build = cxx_build::bridge("src/lib.rs");
 
     for include_path in lhapdf.include_paths {
-        cpp_build.include(include_path);
+        build.include(include_path);
     }
 
-    cpp_build.build("src/lib.rs");
+    build.compile("lhapdf-rust-cxx-bridge");
 
     for lib_path in lhapdf.link_paths {
         println!("cargo:rustc-link-search={}", lib_path.to_str().unwrap());
@@ -19,7 +19,6 @@ fn main() {
     for lib in lhapdf.libs {
         println!("cargo:rustc-link-lib={}", lib);
     }
-}
 
-#[cfg(feature = "docs-only")]
-fn main() {}
+    println!("cargo:rerun-if-changed=src/lib.rs");
+}
